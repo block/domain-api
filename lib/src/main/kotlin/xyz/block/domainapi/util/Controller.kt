4f0fc601e03fe40f43f7a8ef30dde5e11bbb083c
Jem Mawson <jmawson@block.xyz>
2025-08-26 13:32:42 +1000

    Merge with internal version HEAD (#4)

--- lib/src/main/kotlin/xyz/block/domainapi/util/Controller.kt
+++ lib/src/main/kotlin/xyz/block/domainapi/util/Controller.kt
@@ -1,10 +1,13 @@
 package xyz.block.domainapi.util
 
 import app.cash.kfsm.State
-import app.cash.kfsm.Value
 import app.cash.kfsm.StateMachine
+import app.cash.kfsm.Value
+import arrow.core.raise.result
+import xyz.block.domainapi.DomainApiError
 import xyz.block.domainapi.Input
 import xyz.block.domainapi.ProcessingState
+import xyz.block.domainapi.ResultCode
 
 /**
  * An abstract class that represents a controller in the implementation of a domain API based on a state machine.
@@ -19,8 +22,33 @@ abstract class Controller<ID, S : State<ID, V, S>, V : Value<ID, V, S>, R>(
    *
    * @param value The process value.
    * @param inputs The inputs sent by the client or another service.
-   * @param ctx A Jooq DSLContext that can be used to perform multiple updates in a transaction.
+   * @param operation The operation that initiated this call.
    * @return Either the updated process value or a list of hurdles that are still missing.
    */
-  abstract fun process(value: V, inputs: List<Input<R>>): Result<ProcessingState<V, R>>
+  fun process(
+    value: V,
+    inputs: List<Input<R>>,
+    operation: Operation
+  ): Result<ProcessingState<V, R>> =
+    result {
+      handleCancelled(value, inputs).bind() ?: processInputs(value, inputs, operation).bind()
+    }
+
+  abstract fun processInputs(
+    value: V,
+    inputs: List<Input<R>>,
+    operation: Operation
+  ): Result<ProcessingState<V, R>>
+
+  open fun handleCancelled(
+    value: V,
+    requirementResults: List<Input<R>>
+  ): Result<ProcessingState<V, R>?> =
+    result {
+      requirementResults.find { it.result == ResultCode.CANCELLED }?.let {
+        raise(DomainApiError.ProcessWasCancelled(it.id.toString()))
+      }
+    }
 }
+
+enum class Operation { CREATE, EXECUTE, RESUME }
