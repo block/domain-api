6f1471e58c2673bf19c7c17a9306d7c52bb1e2f9
Jeremy Mawson <jmawson@block.xyz>
2025-08-26 10:13:35 +1000

    Reconfigure dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,12 @@ plugins {
   alias(libs.plugins.versionsGradlePlugin)
   alias(libs.plugins.versionCatalogUpdateGradlePlugin)
   alias(libs.plugins.kotlinBinaryCompatibilityPlugin) apply false
+  id("org.jetbrains.dokka") version "1.9.10"
 }
 
 subprojects {
+  apply(plugin = "org.jetbrains.dokka")
+  
   tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     compilerOptions {
       jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
